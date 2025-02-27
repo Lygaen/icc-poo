3167330e6affe94fff36390ef30926197592e4a4
Mathieu Cayeux <mathieucayeux@gmail.com>
2025-02-27 10:38:56 +0100

    Added walls

--- src/gameview.py
+++ src/gameview.py
@@ -4,6 +4,7 @@ class GameView(arcade.View):
     """Main in-game view."""
     player_sprite: arcade.Sprite
     player_sprite_list: arcade.SpriteList[arcade.Sprite]
+    wall_list: arcade.SpriteList[arcade.Sprite]
 
     def __init__(self) -> None:
         # Magical incantion: initialize the Arcade view
@@ -24,9 +25,27 @@ class GameView(arcade.View):
         )
         self.player_sprite_list = arcade.SpriteList()
         self.player_sprite_list.append(self.player_sprite)
+        self.wall_list = arcade.SpriteList(use_spatial_hash=True)
+
+        for i in range(0, 1186, 64):
+            self.wall_list.append(arcade.Sprite(
+                ":resources:images/tiles/grassMid.png",
+                center_x=i,
+                center_y=32,
+                scale=0.5
+            ))
+        
+        for i in [256,512,769]:
+            self.wall_list.append(arcade.Sprite(
+                ":resources:images/tiles/boxCrate_double.png",
+                center_x=i,
+                center_y=96,
+                scale=0.5
+            ))
 
 
     def on_draw(self) -> None:
         """Render the screen."""
         self.clear() # always start with self.clear()   
         self.player_sprite_list.draw()
+        self.wall_list.draw()
