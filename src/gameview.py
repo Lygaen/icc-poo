62aa0e7caa4644771171bd081adb7a4351c732aa
Mathieu Cayeux <mathieucayeux@gmail.com>
2025-02-27 10:32:26 +0100

    Added spritelist

--- src/gameview.py
+++ src/gameview.py
@@ -3,6 +3,7 @@ import arcade
 class GameView(arcade.View):
     """Main in-game view."""
     player_sprite: arcade.Sprite
+    player_sprite_list: arcade.SpriteList[arcade.Sprite]
 
     def __init__(self) -> None:
         # Magical incantion: initialize the Arcade view
@@ -21,9 +22,11 @@ class GameView(arcade.View):
             center_x=64,
             center_y=128
         )
+        self.player_sprite_list = arcade.SpriteList()
+        self.player_sprite_list.append(self.player_sprite)
 
 
     def on_draw(self) -> None:
         """Render the screen."""
         self.clear() # always start with self.clear()   
-        arcade.draw_sprite(self.player_sprite)
+        self.player_sprite_list.draw()
