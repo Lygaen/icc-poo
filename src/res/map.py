01a3efc253195084f6dad7578a45d7dee7d80337
Mathieu Cayeux <mathieucayeux@gmail.com>
2025-03-04 20:38:13 +0100

    Added sprite loading

--- src/res/map.py
+++ src/res/map.py
@@ -1,13 +1,34 @@
 import arcade
 from pathlib import Path
+from enum import Enum
 
-arcade.resources.add_resource_handle("maps", "./maps/")
+arcade.resources.add_resource_handle("maps", Path("./assets/maps/").resolve())
+
+MAP_GRID_SIZE = 64
+MAP_SCALE_FACTOR = 0.5
 
 class Map:
     __path: Path
     __player_pos: arcade.Vec2
     __walls: arcade.SpriteList[arcade.Sprite]
     __interactables: arcade.SpriteList[arcade.Sprite]
+
+    class ObjectType(Enum):
+        WALL = 1
+        COIN = 2
+        MONSTER = 3
+        NOGO = 4
+        START = 5
+
+    MAP_CHAR_INFO: dict[str, tuple[str, ObjectType]] = {
+        "=": (":resources:/images/tiles/grassMid.png", ObjectType.WALL),
+        "-": (":resources:/images/tiles/grassHalf_mid.png", ObjectType.WALL),
+        "x": (":resources:/images/tiles/boxCrate_double.png", ObjectType.WALL),
+        "*": (":resources:/images/items/coinGold.png", ObjectType.COIN),
+        "o": (":resources:/images/enemies/slimeBlue.png", ObjectType.MONSTER),
+        "£": (":resources:/images/tiles/lava.png", ObjectType.NOGO),
+        "S": (":resources:images/animated_characters/female_adventurer/femaleAdventurer_idle.png", ObjectType.START),
+    }
 
     def __init__(self, path: str):
         self.__path = arcade.resources.resolve(":maps:" + path)
@@ -37,13 +58,48 @@ class Map:
     def player_pos(self) -> arcade.Vec2:
         return self.__player_pos
     
-    def __parse_map(self, map: str, size: arcade.Vec2) -> None:
-        pass
+    def __parse_map(self, map: str, size: arcade.Vec2, start: arcade.Vec2 = arcade.Vec2(0,0)) -> None:
+        lines = map.splitlines()
+
+        if len(lines) - 1 > size.y:
+            raise ValueError("Invalid map height")
         
+        lines = lines[:int(size.y)-1]
+        lines.reverse() # So that we loop bottom-up
+
+        for y, line in enumerate(lines):
+            if len(line) > size.x:
+                raise ValueError(f"Invalid map width at line {y}")
+            for x, char in enumerate(line):
+                if char == " ":
+                    continue
+
+                pos = start + arcade.Vec2(x * MAP_GRID_SIZE, y * MAP_GRID_SIZE)
+                info = self.MAP_CHAR_INFO[char]
+
+                sprite = arcade.Sprite(info[0], 
+                                scale=MAP_SCALE_FACTOR,
+                                center_x=pos.x,
+                                center_y=pos.y)
+
+                match info[1]:
+                    case Map.ObjectType.START:
+                        # TODO Player Start
+                        pass
+                    case Map.ObjectType.MONSTER:
+                        # TODO Monster Start
+                        pass
+                    case Map.ObjectType.COIN | Map.ObjectType.NOGO:
+                        self.__interactables.append(sprite)
+                    case Map.ObjectType.WALL:
+                        self.__walls.append(sprite)
+
     def __parse_header(self, header: str) -> arcade.Vec2:
         width: int
         height: int
         for line in header.splitlines():
+            if line.lstrip() == "":
+                continue
             vname = line.split(":", 1)[0]
             vvalue = line.split(":", 1)[1]
 
