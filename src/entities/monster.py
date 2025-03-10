66af1f6c8e427c8efec8e2ad652cd17b462dc9fd
Martin Gremeaux-Bader <martin.gremeaux-bader@epfl.ch>
2025-03-10 16:14:08 +0100

    classe parent monster (mise en commentaires)

--- src/entities/monster.py
+++ src/entities/monster.py
@@ -15,7 +15,36 @@ class Dir(Enum):                #this is an enumeration type of cardinal directi
     back = 5
     backdown = 6
     backup = 7
-# TODO commentaires et docstrings
+
+
+# class Monster(GameObject):
+#     direction : int
+#     gameover_sound: arcade.Sound
+#     """Sound for when player touches the slime
+#     """
+
+#     def __init__(self, map: Map, **kwargs: Any) -> None:
+#         super().__init__(map, ":resources:/images/enemies/slimeBlue.png", **kwargs)
+    
+    
+#     def check_collision(self, dir : Dir)-> bool:
+#         """To check if there is a collider in the immediate neighborhood of the slime along a certain direction (in argument)
+#         """
+#         circle = arcade.SpriteCircle(1, arcade.color.WHITE)         #create a little circle, which we will place in the zone where we want to check for collisions. this allows us to check collisions on more precise parts of the sprite, and not always the sprite in a whole
+#         match dir:                                                  #place the little circle at the extremity of the sprite in the direction wanted. for the diagonals, put it a little further than the edges of the hitboxes so the result is not corrupted by the potentials colliders under and on the sides (but not diagonally) of the slime
+#             case Dir.down:
+#                 circle.position = (self.position[0], self.position[1] - self.size[1]//2)
+#             case Dir.face:
+#                 circle.position = (self.position[0] + self.scale_x*self.direction*self.size[0]//2, self.position[1])
+#             case Dir.facedown:
+#                 circle.position = (self.position[0] + self.scale_x*self.direction*(self.size[0]//2 + 2), self.position[1] - self.size[1]//2 - 1)
+#             case _:
+#                 #pas encore implémenté (pas de besoin pour le moment)
+#                 return False
+#         return (len(arcade.check_for_collision_with_list(circle, self.map.physics_colliders_list)) != 0)        #return true if there is at least one collider in collision with the little circle, false otherwise
+
+
+
 class Slime(GameObject):
     direction : int
     gameover_sound: arcade.Sound
