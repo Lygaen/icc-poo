852eb99ecf3363a4630748735f904efa73787d18
Mathieu Cayeux <mathieucayeux@gmail.com>
2025-03-20 09:01:37 +0100

    Added bow GO

--- src/entities/player.py
+++ src/entities/player.py
@@ -1,12 +1,12 @@
-from typing import Any, cast
+import math
+from typing import Any
+
 import arcade
 
 from src.entities.gameobject import DamageSource, GameObject
 from src.res.map import Map
 
-import math
-
-PLAYER_MOVEMENT_SPEED : int = 3
+PLAYER_MOVEMENT_SPEED: int = 3
 """Lateral speed of the player, in pixels per frame."""
 
 PLAYER_JUMP_SPEED = 12
@@ -28,10 +28,74 @@ SWORD_DOT_DAMAGE = 50
 """Sword damage-over-time.
 """
 
+
+class Bow(GameObject):
+    __mouse_position: arcade.types.Point2
+    """Mouse position, used for calculating the bow position/rotation.
+    """
+
+    def __init__(self, map: list[Map], **kwargs: Any) -> None:
+        super().__init__(map, "assets/bow.png", **kwargs)
+        self.scale = (SWORD_SCALE, SWORD_SCALE)
+        self.event_listener = True
+        self.visible = False
+
+    def update(self, delta_time: float = 1 / 60, *args: Any, **kwargs: Any) -> None:
+        super().update(delta_time, *args, **kwargs)
+
+        if not self.visible:
+            return
+
+        mouse = self.camera.unproject(self.__mouse_position)
+        dir = arcade.Vec2(mouse.x - self.position[0], mouse.y - self.position[1])
+        dir = dir.normalize()
+
+        SCALE_FACTOR = 0.2
+
+        if dir.x < 0:
+            start_pos = (
+                self.map.player.left,
+                self.map.player.center_y - self.map.player.size[1] // 3,
+            )
+        else:
+            start_pos = (
+                self.map.player.right,
+                self.map.player.center_y - self.map.player.size[1] // 3,
+            )
+
+        self.position = (
+            start_pos[0] + dir[0] * self.size[0] * SCALE_FACTOR,
+            start_pos[1] + dir[1] * self.size[1] * SCALE_FACTOR,
+        )
+
+        angle = math.asin(dir.x) - (3 * math.pi / 4)
+
+        if dir.y < 0:
+            angle = -angle - (math.pi / 2)
+        self.radians = angle
+
+    def on_mouse_press(self, x: int, y: int, button: int, modifiers: int) -> None:
+        self.__mouse_position = (x, y)
+        self.visible = True
+
+    def on_mouse_release(self, x: int, y: int, button: int, modifiers: int) -> None:
+        self.__mouse_position = (x, y)
+        self.visible = False
+
+    def on_mouse_motion(self, x: int, y: int, dx: int, dy: int) -> None:
+        self.__mouse_position = (x, y)
+
+
 class Sword(GameObject):
+    __mouse_position: arcade.types.Point2
+    """Mouse position, used for calculating the sword position/rotation.
+    """
+
     def __init__(self, map: list[Map], **kwargs: Any) -> None:
         super().__init__(map, "assets/sword_silver.png", **kwargs)
         self.scale = (SWORD_SCALE, SWORD_SCALE)
+        self.event_listener = True
+        self.visible = False
 
     def update(self, delta_time: float = 1 / 60, *args: Any, **kwargs: Any) -> None:
         super().update(delta_time, *args, **kwargs)
@@ -42,9 +106,48 @@ class Sword(GameObject):
         for hits in self.map.check_for_collisions_all(self):
             hits.on_damage(DamageSource.PLAYER, SWORD_DOT_DAMAGE * delta_time)
 
+        mouse = self.camera.unproject(self.__mouse_position)
+        dir = arcade.Vec2(mouse.x - self.position[0], mouse.y - self.position[1])
+        dir = dir.normalize()
+
+        SCALE_FACTOR = 0.4
+        if dir.x < 0:
+            start_pos = (
+                self.map.player.left,
+                self.map.player.center_y - self.map.player.size[1] // 3,
+            )
+        else:
+            start_pos = (
+                self.map.player.right,
+                self.map.player.center_y - self.map.player.size[1] // 3,
+            )
+
+        self.position = (
+            start_pos[0] + dir[0] * self.size[0] * SCALE_FACTOR,
+            start_pos[1] + dir[1] * self.size[1] * SCALE_FACTOR,
+        )
+
+        angle = math.asin(dir.x) - (math.pi / 4)
+
+        if dir.y < 0:
+            angle = -angle + (math.pi / 2)
+        self.radians = angle
+
+    def on_mouse_press(self, x: int, y: int, button: int, modifiers: int) -> None:
+        self.__mouse_position = (x, y)
+        self.visible = True
+
+    def on_mouse_release(self, x: int, y: int, button: int, modifiers: int) -> None:
+        self.__mouse_position = (x, y)
+        self.visible = False
+
+    def on_mouse_motion(self, x: int, y: int, dx: int, dy: int) -> None:
+        self.__mouse_position = (x, y)
+
+
 class Player(GameObject):
-    """The main player game object.
-    """
+    """The main player game object."""
+
     is_move_initiated: bool
     """Whether the move was initiated (the key was pressed)
     on a frame where the player was present.
@@ -68,18 +171,18 @@ class Player(GameObject):
     base_HP: float = 20
     """Base healtpoint of the player.
     """
-    __mouse_position: arcade.types.Point2
-    """Mouse position, used for calculating the sword position/rotation.
-    """
 
     __buffered_jump_timer: float = 0
     __coyote_timer: float = 0
 
     def __init__(self, map: list[Map], **kwargs: Any) -> None:
-        """Initializes the player tl;dr see GameObject#__init__
-        """
+        """Initializes the player tl;dr see GameObject#__init__"""
 
-        super().__init__(map, ":resources:images/animated_characters/female_adventurer/femaleAdventurer_idle.png", **kwargs)
+        super().__init__(
+            map,
+            ":resources:images/animated_characters/female_adventurer/femaleAdventurer_idle.png",
+            **kwargs,
+        )
 
         # Ugly bug where arcade didn't register on my laptop
         # when a key was pressed on specific frames but registered
@@ -90,8 +193,7 @@ class Player(GameObject):
         self.gameover_sound = arcade.Sound(":resources:sounds/gameover1.wav")
         self.event_listener = True
         self.sword = Sword(map)
-        self.sword.visible = False
-        self.__mouse_position = arcade.Vec2(0, 0)
+
         self.__buffered_jump_timer = 0
         self.__coyote_timer = 0
         self.HP = self.base_HP
@@ -112,7 +214,9 @@ class Player(GameObject):
     def on_key_release(self, symbol: int, modifiers: int) -> None:
         match symbol:
             case arcade.key.RIGHT:
-                if self.is_move_initiated: # See in __init__ for explanation (yes, there is one)
+                if (
+                    self.is_move_initiated
+                ):  # See in __init__ for explanation (yes, there is one)
                     self.change_x -= PLAYER_MOVEMENT_SPEED
             case arcade.key.LEFT:
                 if self.is_move_initiated:
@@ -132,38 +236,7 @@ class Player(GameObject):
             self.game_view.score = 0
             self.map.reload()
 
-    def on_mouse_press(self, x: int, y: int, button: int, modifiers: int) -> None:
-        self.__mouse_position = (x, y)
-        self.sword.visible = True
-
-    def on_mouse_release(self, x: int, y: int, button: int, modifiers: int) -> None:
-        self.__mouse_position = (x, y)
-        self.sword.visible = False
-
-    def on_mouse_motion(self, x: int, y: int, dx: int, dy: int) -> None:
-        self.__mouse_position = (x, y)
-
     def update(self, delta_time: float = 1 / 60, *args: Any, **kwargs: Any) -> None:
-        mouse = self.camera.unproject(self.__mouse_position)
-        dir = arcade.Vec2(mouse.x - self.map.player.position[0], mouse.y - self.map.player.position[1])
-        dir = dir.normalize()
-        SCALE_FACTOR = 0.4
-        start_pos = self.position
-
-        if dir.x < 0: # Move the sword next to the player's hand, depending on side
-            start_pos = (self.left, self.center_y - self.size[1] // 3)
-        else:
-            start_pos = (self.right, self.center_y - self.size[1] // 3)
-
-        self.sword.position = (start_pos[0] + dir[0] * self.sword.size[0] * SCALE_FACTOR,
-            start_pos[1] + dir[1] * self.sword.size[1] * SCALE_FACTOR)
-
-        angle = math.asin(dir.x) - (math.pi / 4) # Rotate the sword
-
-        if dir.y < 0:
-            angle = -angle + (math.pi / 2)
-        self.sword.radians = angle
-
         super().update(delta_time, *args, **kwargs)
 
         on_ground = self.map.physics_engine.can_jump()
@@ -183,5 +256,5 @@ class Player(GameObject):
             self.__buffered_jump_timer -= delta_time
 
     def destroy(self) -> None:
-        self.sword.destroy() # Destroy the sword as well
+        self.sword.destroy()  # Destroy the sword as well
         super().destroy()
