ea472de4364f3df5aaf22f3f0fa83997369d505b
Martin Gremeaux-Bader <martin.gremeaux-bader@epfl.ch>
2025-05-15 09:47:05 +0200

    void damage correction

--- src/entities/player.py
+++ src/entities/player.py
@@ -351,7 +351,7 @@ class Player(GameObject):
 
     def on_damage(self, source: DamageSource, damage: float) -> bool:
         match source:
-            case DamageSource.LAVA:
+            case DamageSource.LAVA | DamageSource.VOID:
                 self.HP = 0
             case DamageSource.MONSTER:
                 self.HP -= damage
@@ -367,7 +367,7 @@ class Player(GameObject):
     def update(self, delta_time: float = 1 / 60, *args: Any, **kwargs: Any) -> None:
         super().update(delta_time, *args, **kwargs)
 
-        if self.center_y < -10 :
+        if self.center_y < -500 :
             self.on_damage(DamageSource.VOID, self.HP)
 
         on_ground = self.map.physics_engine.can_jump()
