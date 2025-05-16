ecd3fdb8379dcb4cdab5a3ab8afa89d0813b7016
Martin Gremeaux-Bader <martin.gremeaux-bader@epfl.ch>
2025-05-16 15:16:37 +0200

    knockback correction

--- src/entities/player.py
+++ src/entities/player.py
@@ -367,7 +367,7 @@ class Player(GameObject):
         self.center_y += self.__knockback[1]*delta_time
         for i in range(len(self.__knockback)):
             self.__knockback[i] *= 0.7
-            if self.__knockback[i] < 0.01:
+            if abs(self.__knockback[i]) < 0.01:
                 self.__knockback[i] = 0
 
     def update(self, delta_time: float = 1 / 60, *args: Any, **kwargs: Any) -> None:
